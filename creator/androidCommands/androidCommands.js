0ef6000146eaaea11f445b80375526741e12c425
Francisco <gammafp@gmail.com>
2019-07-08 22:28:04 +0200

    Clear messages

--- creator/androidCommands/androidCommands.js
+++ creator/androidCommands/androidCommands.js
@@ -1,20 +1,27 @@
 const command = require('child_process');
+const ora = require('ora');
+
 module.exports = {
     copy: () => {
-        const exec = command.spawn('npm', [
-            'run',
-            'build-install-android',
-            '&&',
-            'npx',
-            'cap',
-            'copy'
-        ], {shell: true});
-
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
-        exec.stderr.on('data', function(data) {
-            console.log(data.toString());
+        const spinner = ora(`Updating the project of Android Studio...\n`);
+        spinner.start();
+        command.exec('npm run build-install-android', (err, stdout, stderr) => {
+            if (err) {
+                log.danger(err);
+                return;
+            }
+            spinner.succeed();
+            const exec = command.spawn('npx', [
+                'cap',
+                'copy'
+            ], {shell: true});
+    
+            exec.stdout.on('data', function(data) {
+                console.log(data.toString());
+            });
+            exec.stderr.on('data', function(data) {
+                console.log(data.toString());
+            });
         });
     }
 }
